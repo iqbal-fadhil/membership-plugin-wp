45e80d05a167f7f0bf4668d8f7bb3780a3a4eedf
Iqbal Fadhil <1445518-iqbal-fadhil@users.noreply.gitlab.com>
2024-10-10 12:07:12 +0700

    Initial commit

---
+++ membership-plugin.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: Membership Plugin
+ * Description: This plugin acts as a custom membership plugin with Bootstrap admin view.
+ * Version: 1.0
+ * Author: Iqbal Fadhil (https://iqbalfadhil.com)
+ */
